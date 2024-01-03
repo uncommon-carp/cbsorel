31ded5fac991f24e774aec8ca19e8008c737ffcf
Cory Sorel <csorel.creative@gmail.com>
2024-01-03 07:27:21 -0600

    refactor to use portfolio card

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,15 +5,7 @@ export default function Portfolio() {
     <main className="flex min-h-screen flex-col items-center">
       <div className="flex flex-row w-3/5 my-auto flex-wrap">
         <Card title="FIPS API" thumb="" />
-        <div className="w-1/3 m-4">
-          <h2>Pescador</h2>
-        </div>
-        <div className="w-1/3 m-4">
-          <h2>FIPS County Codes</h2>
-        </div>
-        <div className="w-1/3 m-4">
-          <h2>Pescador</h2>
-        </div>
+        <Card title="Pescador" thumb="" />
       </div>
     </main>
   );
