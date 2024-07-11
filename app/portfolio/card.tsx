f0ddd7d878915f60ffadbf352a866c2a588f8432
Cory Sorel <csorel.creative@gmail.com>
2024-07-11 00:27:48 -0500

    add url link

--- app/portfolio/card.tsx
+++ app/portfolio/card.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface CardProps {
   title: string;
   thumb: string;
+  url: string;
 }
 
 export default function Card(props: CardProps) {
   return (
     <div className="w-1/3 m-4">
       <Image src={props.thumb} alt="Project thumb" />
-      <h2>{props.title}</h2>
+      <Link href={props.url ?? "#"}>
+        <h2>{props.title}</h2>
+      </Link>
     </div>
   );
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,8 +4,12 @@ export default function Portfolio() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <div className="flex flex-row w-3/5 my-auto flex-wrap">
-        <Card title="FIPS API" thumb="" />
-        <Card title="Pescador" thumb="" />
+        <Card
+          title="FIPS API"
+          thumb=""
+          url="https://fips-code-api.herokuapp.com"
+        />
+        <Card title="Pescador" thumb="" url="" />
       </div>
     </main>
   );
